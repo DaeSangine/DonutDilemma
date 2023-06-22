7df1ca7ed95b66fa285276e61eeb07a279d71c43
DaeSanguine <134565877+DaeSangine@users.noreply.github.com>
2023-06-23 09:39:06 +1200

    Making prisoners dilemma

---
+++ src/Donut_Dilema.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class DonutDilemma
+{
+    // instance variables - replace the example below with your own
+    public static int[][] board = new int[10][10];
+    public static Scanner inputStream = new Scanner(System.in);
+    public static Scanner keyboard=new Scanner(System.in);
+
+
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String userName = keyboard.nextLine();
+        System.out.println("Player's name is: " + userName);
+        // Code from w3s
+    }
+}
+
