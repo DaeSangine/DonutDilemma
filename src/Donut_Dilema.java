310c62bfd3efc0f672209c915c52148a050ead6d
DaeSanguine <134565877+DaeSangine@users.noreply.github.com>
2023-07-20 13:18:49 +1200

    Introductions

--- src/Donut_Dilema.java
+++ src/Donut_Dilema.java
@@ -9,8 +9,8 @@ class DonutDilemma
         // some code from w3s
         String userName = keyboard.nextLine();
         System.out.println("Welcome to my dilemma " + userName); // getting username
-        System.out.println("Your friend and you are walking through a town when you come across a lone donut place.");
-        System.out.println("");
+        System.out.println("Your friend and you are walking through a town when you come across a donut place.\nOne of you is feeling a bit hungry so you end up going in.\nAt the register you can see up on a board that the donuts cost $8 each.");
+
     }
 }
 
