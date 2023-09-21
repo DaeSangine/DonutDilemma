370d415ce5027aa269eeb145ca4691132d5b1dba
DaeSanguine <134565877+DaeSangine@users.noreply.github.com>
2023-09-22 09:18:00 +1200

    Here comes the moneyyyyy

--- src/DonutDilema.java
+++ src/DonutDilema.java
@@ -55,8 +55,10 @@ class DonutDilemma {
             }
         } while (numberOfRounds == -1);
 
+        //Get money
+        int m = (5*numberOfRounds-5);
         // Play the game
-        print("Your friend and you are walking through a town when you come across a donut shop.\nOne of you is feeling a bit hungry so you end up going in.\nAt the register you can see up on a board that the donuts cost $8 each.");
+        print("Your friend and you are walking through a town when you come across a donut shop.\nOne of you is feeling a bit hungry so you end up going in.\nAt the register you can see up on a board that the donuts cost $5 each. You have $"+m+".");
         for (int i = 0; i<numberOfRounds; ++i) {
             print("You are faced with a choice, do you spend the little money that you have on a donut, or do you save your money for something more important? Spend/Save");
             String answer = keyboard.nextLine().toLowerCase();
