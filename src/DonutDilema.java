ad8cbdaa9b8772039a272f04e17c81683591e66e
DaeSangine <lachlanbyrnenz@gmail.com>
2023-10-11 20:17:42 +1300

    Spending money

--- src/DonutDilema.java
+++ src/DonutDilema.java
@@ -1,3 +1,12 @@
+/**
+ * Title: Donut Dilemma
+ * Author: Lachlan Byrne
+ * Date: 11/10/2023
+ * Version: 1.0
+ * Purpose: Year 12 CSC project, making a game in javascript.
+ */
+
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -56,15 +65,23 @@ class DonutDilemma {
         } while (numberOfRounds == -1);
 
         //Get money
-        int m = (5*numberOfRounds-5);
+        int money = (5*numberOfRounds-5);
         // Play the game
-        print("Your friend and you are walking through a town when you come across a donut shop.\nOne of you is feeling a bit hungry so you end up going in.\nAt the register you can see up on a board that the donuts cost $5 each. You have $"+m+".");
+        print("Your friend and you are walking through a town when you come across a donut shop.\nOne of you is feeling a bit hungry so you end up going in.\nAt the register you can see up on a board that the donuts cost $5 each. You have $"+money+".");
+        print("You are faced with a choice, do you spend the little money that you have on a donut, or do you save your money for something more important?");
         for (int i = 0; i<numberOfRounds; ++i) {
-            print("You are faced with a choice, do you spend the little money that you have on a donut, or do you save your money for something more important? Spend/Save");
+            if (money <= 0) {
+                print("You have run out of money :(");
+                break;
+            }
+
+            print("You have $"+money+" left. Spend/Save");
             String answer = keyboard.nextLine().toLowerCase();
 
             if (answer.equals("spend")) {
                 print("You have chosen to buy a donut.");
+                // Taking away money
+                money -= 5;
                 choices.add(answer);
             } else if (answer.equals("save")) {
                 print("You have decided to save your money.");
